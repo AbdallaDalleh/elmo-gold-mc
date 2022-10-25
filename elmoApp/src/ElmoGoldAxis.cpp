9bae901b749bb26ec750a6074f4c9df435188f16
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2022-10-25 15:50:01 +0300

    Added very basic axis class implementation with basic functions.

--- elmoApp/src/ElmoGoldAxis.cpp
+++ elmoApp/src/ElmoGoldAxis.cpp
@@ -8,3 +8,51 @@ ElmoGoldAxis::ElmoGoldAxis(ElmoGoldController* _controller, int axis_number)
 
 }
 
+asynStatus ElmoGoldAxis::move(double position, int relative, double minVelocity, double maxVelocity, double acceleration)
+{
+    if(relative)
+        sprintf(controller->outString_, "PR=%d", position);
+    else
+        sprintf(controller->outString_, "PA=%d", position);
+    controller->writeController();
+    sprintf(controller->outString_, "BG");
+    controller->writeController();
+
+    return asynSuccess;
+}
+
+asynStatus ElmoGoldAxis::stop(double acceleration)
+{
+    int status;
+
+    sprintf(controller->outString_, "SD=%d", (int) acceleration);
+    status = controller->writeController();
+    if(status != asynSuccess)
+        return status;
+
+    sprintf(controller->outString_, "ST");
+    status = controller->writeController();
+    if(status != asynSuccess)
+        return status;
+
+    return asynSuccess;
+}
+
+asynStatus ElmoGoldAxis::setPosition(double position)
+{
+    int status;
+
+    sprintf(controller->outString_, "PA=%f", position);
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::poll(bool* moving)
+{
+    int status;
+
+    sprintf(controller->outString_, "FP");
+    controller->writeReadController();
+    setDoubleParam(controller->motorPosition_, atof(controller->inString_));
+}
+
--- elmoApp/src/ElmoGoldAxis.h
+++ elmoApp/src/ElmoGoldAxis.h
@@ -13,6 +13,10 @@ class ElmoGoldAxis : public asynMotorAxis
 {
 public:
     ElmoGoldAxis(class ElmoGoldController* _controller, int axis_number);
+    asynStatus move(double position, int relative, double minVelocity, double maxVelocity, double acceleration);
+    asynStatus stop(double acceleration);
+    asynStatus poll(bool* moving);
+    asynStatus setPosition(double position);
 
 private:
     ElmoGoldController* controller;
