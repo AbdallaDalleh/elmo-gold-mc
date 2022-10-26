bc0bd0137c5a2d4c729a848af49c3802227249ed
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2022-10-26 13:29:36 +0300

    Added jog, limits, and PI control commands.

--- elmoApp/src/ElmoGoldAxis.cpp
+++ elmoApp/src/ElmoGoldAxis.cpp
@@ -11,10 +11,21 @@ ElmoGoldAxis::ElmoGoldAxis(ElmoGoldController* _controller, int axis_number)
 asynStatus ElmoGoldAxis::move(double position, int relative, double minVelocity, double maxVelocity, double acceleration)
 {
     if(relative)
-        sprintf(controller->outString_, "PR=%d", position);
+        sprintf(controller->outString_, "PR=%ld;BG;", NINT(position));
     else
-        sprintf(controller->outString_, "PA=%d", position);
+        sprintf(controller->outString_, "PA=%ld;BG;", NINT(position));
     controller->writeController();
+
+    return asynSuccess;
+}
+
+asynStatus ElmoGoldAxis::moveVelocity(double minVelocity, double maxVelocity, double acceleration)
+{
+    long iAccel = NINT(acceleration);
+    
+    sprintf(controller->outString_, "AC=%ld;DC=%ld;JV=%ld;FS=%ld;", iAccel, iAccel, NINT(minVelocity), NINT(maxVelocity));
+    controller->writeController();
+    
     sprintf(controller->outString_, "BG");
     controller->writeController();
 
@@ -23,9 +34,9 @@ asynStatus ElmoGoldAxis::move(double position, int relative, double minVelocity,
 
 asynStatus ElmoGoldAxis::stop(double acceleration)
 {
-    int status;
+    asynStatus status;
 
-    sprintf(controller->outString_, "SD=%d", (int) acceleration);
+    sprintf(controller->outString_, "SD=%ld", NINT(acceleration));
     status = controller->writeController();
     if(status != asynSuccess)
         return status;
@@ -40,19 +51,68 @@ asynStatus ElmoGoldAxis::stop(double acceleration)
 
 asynStatus ElmoGoldAxis::setPosition(double position)
 {
-    int status;
+    asynStatus status;
 
-    sprintf(controller->outString_, "PA=%f", position);
+    sprintf(controller->outString_, "PX=%ld", NINT(position));
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::setEncoderPosition(double position)
+{
+    asynStatus status;
+
+    sprintf(controller->outString_, "FP=%ld", NINT(position));
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::setHighLimit(double highLimit)
+{
+    asynStatus status;
+
+    sprintf(controller->outString_, "VH[3]=%ld", NINT(highLimit));
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::setLowLimit(double lowLimit)
+{
+    asynStatus status;
+
+    sprintf(controller->outString_, "VL[3]=%ld", NINT(lowLimit));
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::setPGain(double pGain)
+{
+    asynStatus status;
+
+    sprintf(controller->outString_, "KP[1]=%f", pGain);
+    status = controller->writeController();
+    return status;
+}
+
+asynStatus ElmoGoldAxis::setIGain(double iGain)
+{
+    asynStatus status;
+
+    sprintf(controller->outString_, "KI[1]=%f", iGain);
     status = controller->writeController();
     return status;
 }
 
 asynStatus ElmoGoldAxis::poll(bool* moving)
 {
-    int status;
+    asynStatus status;
 
-    sprintf(controller->outString_, "FP");
+    sprintf(controller->outString_, "PA");
     controller->writeReadController();
     setDoubleParam(controller->motorPosition_, atof(controller->inString_));
+
+    sprintf(controller->outString_, "PX");
+    controller->writeReadController();
+    setDoubleParam(controller->motorEncoderPosition_, atof(controller->inString_));
 }
 
--- elmoApp/src/ElmoGoldAxis.h
+++ elmoApp/src/ElmoGoldAxis.h
@@ -5,6 +5,7 @@
 
 #include "asynMotorController.h"
 #include "asynMotorAxis.h"
+#include "motor.h"
 
 using std::cout;
 using std::endl;
@@ -14,9 +15,15 @@ class ElmoGoldAxis : public asynMotorAxis
 public:
     ElmoGoldAxis(class ElmoGoldController* _controller, int axis_number);
     asynStatus move(double position, int relative, double minVelocity, double maxVelocity, double acceleration);
+    asynStatus moveVelocity(double minVelocity, double maxVelocity, double acceleration);
     asynStatus stop(double acceleration);
     asynStatus poll(bool* moving);
     asynStatus setPosition(double position);
+    asynStatus setEncoderPosition(double position);
+    asynStatus setHighLimit(double highLimit);
+    asynStatus setLowLimit(double lowLimit);
+    asynStatus setPGain(double pGain);
+    asynStatus setIGain(double iGain);
 
 private:
     ElmoGoldController* controller;
